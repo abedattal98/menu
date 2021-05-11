feba2e50e1dff26a3c732904623a19aabd6db0ad
AbdelRahman MassriAttal <abed_attal@hotmail.com>
2021-05-11 23:41:40 +0300

    change about-li

--- src/app/about-section/about-section.component.ts
+++ src/app/about-section/about-section.component.ts
@@ -10,26 +10,27 @@ export class AboutSectionComponent implements OnInit {
   @Input() role = null;
   @Input() image = null;
   @Input() icon = null;
-
+  @Input() list = null
 
 
   Users = [];
-  list =[]
-  constructor() {
-    fetch('https://js-test-abed.herokuapp.com/rawaa')
-    .then(response => response.json())
-    .then(json =>{
-      this.Users =json.Sheet1;
-      this.name=json.Sheet1[0].name
-      this.role=json.Sheet1[0].role
-      this.icon=json.Sheet1[0].icon
-      this.image=json.Sheet1[0].image
-    })
-    fetch('https://obscure-retreat-73939.herokuapp.com/rawaa')
-    .then(response => response.json())
-    .then(json =>{
-      this.list =json;
-    })
+  
+  
+  constructor() { 
+    // fetch('https://js-test-abed.herokuapp.com/rawaa')
+    // .then(response => response.json())
+    // .then(json =>{
+    //   this.Users =json.Sheet1;
+    //   this.name=json.Sheet1[0].name
+    //   this.role=json.Sheet1[0].role
+    //   this.icon=json.Sheet1[0].icon
+    //   this.image=json.Sheet1[0].image
+    // })
+    // fetch('https://obscure-retreat-73939.herokuapp.com/rawaa')
+    // .then(response => response.json())
+    // .then(json =>{
+    //   this.list =json;
+    // })
    }
 
   ngOnInit(): void {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,36 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  Users=[]
+  list =[]
+  listC =[]
+  name=''
+  role=''
+  icon=''
+  image=''
+  
   title = 'ang';
+  constructor(){
+  fetch('https://js-test-abed.herokuapp.com/rawaa')
+  .then(response => response.json())
+  .then(json =>{
+    this.Users =json.Sheet1;
+    this.name=json.Sheet1[0].name
+    this.role=json.Sheet1[0].role
+    this.icon=json.Sheet1[0].icon
+    this.image=json.Sheet1[0].image
+  })
+  fetch('https://obscure-retreat-73939.herokuapp.com/rawaa')
+  .then(response => response.json())
+  .then(json =>{
+    this.list =json;
+  })
+  fetch('https://restcountries.eu/rest/v2/all')
+  .then(response => response.json())
+  .then(json =>{
+    this.listC =json;
+  })
+}
 }
