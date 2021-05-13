22b4048678437fb0292a823577392a26394c7aba
AbdelRahman MassriAttal <abed_attal@hotmail.com>
2021-05-13 07:24:01 +0300

    change json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,16 +20,15 @@ export class AppComponent {
   .then(response => response.json())
   .then(json =>{
     this.User =json.Sheet1[0];
-    this.Why =json.Sheet2;
-
-   
+    this.Why =json.Sheet4;
+    this.Menu=json.Sheet2;
+    this.list =json.Sheet3;
   })
   
   fetch('https://obscure-retreat-73939.herokuapp.com/rawaa')
   .then(response => response.json())
   .then(json =>{
-    this.list =json;
-    this.Menu=json;
+  //  this.Menu=json;
   })
   // fetch('https://restcountries.eu/rest/v2/all')
   // .then(response => response.json())
